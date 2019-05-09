night-codes/WebKit

commit 186143dd4b47ea4fde52adc520f5898fc0463d23

AX: display:none content exposed to accessibility when aria-hidden is toggled on ancestor element https://bugs.webkit.org/show_bug.cgi?id=139142
Reviewed by Darin Adler.
Source/WebCore:


Amend the code that determines when an invisible, but aria-hidden=false, element is exposed to accessibility.

The new guideline is that you must have aria-hidden=false on every node that is not rendered (except text which inherits)
otherwise the element will not be visible.

Modified existing test: accessibility/aria-hidden-false-works-in-subtrees.html

* accessibility/AXObjectCache.cpp:
(WebCore::isNodeAriaVisible):
(WebCore::AXObjectCache::rootWebArea):

LayoutTests:


* accessibility/aria-hidden-false-works-in-subtrees.html:



Identifier: 163406@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@184890 268f45cc-cd09-0410-ab3c-d52691b4dbfc

LayoutTests/accessibility/aria-hidden-false-works-in-subtrees.html
@@ -5,18 +5,57 @@
 </head>
 <body id="body">
 
+<div id="content">
+
 <div hidden aria-hidden="false" aria-label="group0">
    <div aria-hidden="true" aria-label="group1">
       ignore me
    </div>
-   <div role="group" aria-label="group2">
-     <div role="button" aria-label="button1">button</div>
+   <div aria-hidden="false" role="group" aria-label="group2">
+     <div aria-hidden="false" role="button" aria-label="button1">button</div>
      don't ignore me
+  </div>
+</div>
+
+<!-- The text node should be visible because it inherits from the parent -->
+<div id="group3" hidden aria-hidden="false">
+text3
+</div>
+
+<!-- The text node should be visible because it inherits from the parent, and all parents need aria-hidden=false -->
+<div id="group4" hidden aria-hidden="false">
+  <div role="group" hidden aria-hidden="false">
+    text4
+  </div>
+</div>
+
+<!-- The text node should NOT be visible because it inherits from the parent, and all parents need aria-hidden=false -->
+<div id="group5" role="group">
+  <div hidden>
+    <div role="group" hidden aria-hidden="false">
+      text5
+    </div>
+  </div>
+</div>
+
+<!-- The button inside should NOT be visible because it is not marked as aria-hidden -->
+<div id="group6" hidden aria-hidden="false">
+  <div aria-hidden="false">
+     <button>button</button>
+  </div>
+</div>
+
+<!-- The button inside should be visible because it is marked as aria-hidden and all its parents are too -->
+<div id="group7" hidden aria-hidden="false">
+  <div aria-hidden="false">
+     <button aria-hidden="false">button</button>
   </div>
 </div>
 
 <div id="iframe">
 <iframe onload="testiFrameContent();" aria-hidden="false" src="resources/cake.png">hidden content</iframe>
+</div>
+
 </div>
 
 <p id="description"></p>
@@ -33,6 +72,8 @@
         debug("Non-rendered iframe content should not be visible when aria-hidden=true. The first child should be a group and NOT static text.");
         iframeChild = iframe.childAtIndex(0);
         debug("iFrame child role: " + iframeChild.role);
+
+        document.getElementById("content").style.visibility = "hidden";
 
         finishJSTest();
     }
@@ -53,9 +94,36 @@
     if (window.accessibilityController) {
         window.jsTestIsAsync = true;
 
-        var root = accessibilityController.rootElement.childAtIndex(0).childAtIndex(0);
+        var root = accessibilityController.rootElement.childAtIndex(0).childAtIndex(0); 
         dumpAccessibilityChildren(root, 0);
-}
+
+        // Text inside aria-hidden=false inherits from parent.
+        var object = accessibilityController.accessibleElementById("group3").childAtIndex(0);
+        shouldBe("object.role", "'AXRole: AXStaticText'");
+        var stringValue = object.stringValue.replace(/\n/g, '');
+        shouldBe("stringValue", "'AXValue: text3'");
+
+        // Text inside nested aria-hidden=false inherits from parent.
+        object = accessibilityController.accessibleElementById("group4").childAtIndex(0).childAtIndex(0);
+        shouldBe("object.role", "'AXRole: AXStaticText'");
+        stringValue = object.stringValue.replace(/\n/g, '');
+        shouldBe("stringValue", "'AXValue:     text4  '");
+
+        // When not all the parents have aria-hidden=false, element should not be visible.
+        object = accessibilityController.accessibleElementById("group5").childAtIndex(0);
+        shouldBeTrue("!object || !object.isValid");
+
+        // Objects that don't have aria-hidden=false are not visible when the parents have that attribute.
+        object = accessibilityController.accessibleElementById("group6").childAtIndex(0);
+        shouldBe("object.role", "'AXRole: AXGroup'");
+        shouldBe("object.childrenCount", "0");
+
+        // When all objects have aria-hidden=false, then the elements are visible.
+        object = accessibilityController.accessibleElementById("group7").childAtIndex(0);
+        shouldBe("object.role", "'AXRole: AXGroup'");
+        shouldBe("object.childrenCount", "1");
+        shouldBe("object.childAtIndex(0).role", "'AXRole: AXButton'");
+    }
 
 </script>
 

Source/WebCore/accessibility/AXObjectCache.cpp
@@ -1300,20 +1300,30 @@ bool isNodeAriaVisible(Node* node)
 {
     if (!node)
         return false;
-    
-    // To determine if a node is ARIA visible, we need to check the parent hierarchy to see if anyone specifies
-    // aria-hidden explicitly.
+
+    // ARIA Node visibility is controlled by aria-hidden
+    //  1) if aria-hidden=true, the whole subtree is hidden
+    //  2) if aria-hidden=false, and the object is rendered, there's no effect
+    //  3) if aria-hidden=false, and the object is NOT rendered, then it must have
+    //     aria-hidden=false on each parent until it gets to a rendered object
+    //  3b) a text node inherits a parents aria-hidden value
+    bool requiresAriaHiddenFalse = !node->renderer();
+    bool ariaHiddenFalsePresent = false;
     for (Node* testNode = node; testNode; testNode = testNode->parentNode()) {
         if (is<Element>(*testNode)) {
             const AtomicString& ariaHiddenValue = downcast<Element>(*testNode).fastGetAttribute(aria_hiddenAttr);
-            if (equalIgnoringCase(ariaHiddenValue, "false"))
-                return true;
             if (equalIgnoringCase(ariaHiddenValue, "true"))
                 return false;
+            
+            bool ariaHiddenFalse = equalIgnoringCase(ariaHiddenValue, "false");
+            if (!testNode->renderer() && !ariaHiddenFalse)
+                return false;
+            if (!ariaHiddenFalsePresent && ariaHiddenFalse)
+                ariaHiddenFalsePresent = true;
         }
     }
     
-    return false;
+    return !requiresAriaHiddenFalse || ariaHiddenFalsePresent;
 }
 
 AccessibilityObject* AXObjectCache::rootWebArea()

qtwebkit/webkit-mirror

commit ed1593fef20f75c16da82888de6e15b07da14c40

Merge r244187 - [GTK] Layout test accessibility/aria-hidden-false-works-in-subtrees.html fails after r184890 https://bugs.webkit.org/show_bug.cgi?id=146718 <rdar://problem/21722487>
Reviewed by Joanmarie Diggs.

Source/WebCore:

Allow to get the text under element for nodes hidden in DOM but explicitly exposed to accessibility with
aria-hidden="false".

Fixes: accessibility/aria-hidden-false-works-in-subtrees.html

* accessibility/AccessibilityNodeObject.cpp:
(WebCore::AccessibilityNodeObject::textUnderElement const):
* accessibility/atk/WebKitAccessible.cpp:
(roleIsTextType): Also consider ApplicationGroup role as text elements, so that <div>text</div> is equivalent to
<div role="roup">test</div>.

LayoutTests:

Update test to ATK behavior, rebaseline it and remove the expectations.

* accessibility/aria-hidden-false-works-in-subtrees.html:
* platform/gtk/TestExpectations:
* platform/gtk/accessibility/aria-hidden-false-works-in-subtrees-expected.txt:

git-svn-id: http://svn.webkit.org/repository/webkit/releases/WebKitGTK/webkit-2.24@245105 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/accessibility/AccessibilityNodeObject.cpp
@@ -1745,10 +1745,14 @@ String AccessibilityNodeObject::textUnderElement(AccessibilityTextUnderElementMo
     if (is<Text>(node))
         return downcast<Text>(*node).wholeText();
 
+    bool isAriaVisible = AccessibilityObject::matchedParent(*this, true, [] (const AccessibilityObject& object) {
+        return equalLettersIgnoringASCIICase(object.getAttribute(aria_hiddenAttr), "false");
+    }) != nullptr;
+
     // The Accname specification states that if the current node is hidden, and not directly
     // referenced by aria-labelledby or aria-describedby, and is not a host language text
     // alternative, the empty string should be returned.
-    if (isHidden() && !is<HTMLLabelElement>(node) && (node && !ancestorsOfType<HTMLCanvasElement>(*node).first())) {
+    if (isDOMHidden() && !isAriaVisible && !is<HTMLLabelElement>(node) && (node && !ancestorsOfType<HTMLCanvasElement>(*node).first())) {
         AccessibilityObject::AccessibilityChildrenVector labelFor;
         AccessibilityObject::AccessibilityChildrenVector descriptionFor;
         ariaLabelledByReferencingElements(labelFor);

Source/WebCore/accessibility/atk/WebKitAccessible.cpp
@@ -1180,7 +1180,8 @@ static bool roleIsTextType(AccessibilityRole role)
         || role == AccessibilityRole::Pre
         || role == AccessibilityRole::GridCell
         || role == AccessibilityRole::TextGroup
-        || role == AccessibilityRole::ApplicationTextGroup;
+        || role == AccessibilityRole::ApplicationTextGroup
+        || role == AccessibilityRole::ApplicationGroup;
 }
 
 static guint16 interfaceMaskFromObject(AccessibilityObject* coreObject)

LayoutTests/accessibility/aria-hidden-false-works-in-subtrees.html
@@ -2,6 +2,7 @@
 <html>
 <head>
 <script src="../resources/js-test-pre.js"></script>
+<script src="../resources/accessibility-helper.js"></script>
 </head>
 <body id="body">
 
@@ -84,7 +85,7 @@ text3
         }
         var indent = "";
         for (var k = 0; k < level; k++) { indent += "  "; }
-        debug(indent + element.role + "\n" + indent + element.description + "\n");
+        debug(indent + element.role + "\n" + indent + platformValueForW3CName(element, true) + "\n");
         var childrenCount = element.childrenCount;
         for (var k = 0; k < childrenCount; k++) {
             dumpAccessibilityChildren(element.childAtIndex(k), level+1);
@@ -98,16 +99,30 @@ text3
         dumpAccessibilityChildren(root, 0);
 
         // Text inside aria-hidden=false inherits from parent.
-        var object = accessibilityController.accessibleElementById("group3").childAtIndex(0);
-        shouldBe("object.role", "'AXRole: AXStaticText'");
+        var object;
+        if (accessibilityController.platformName == "atk") {
+            object = accessibilityController.accessibleElementById("group3");
+            shouldBe("object.role", "'AXRole: AXSection'");
+        } else {
+            object = accessibilityController.accessibleElementById("group3").childAtIndex(0);
+            shouldBe("object.role", "'AXRole: AXStaticText'");
+        }
         var stringValue = object.stringValue.replace(/\n/g, '');
         shouldBe("stringValue", "'AXValue: text3'");
 
         // Text inside nested aria-hidden=false inherits from parent.
-        object = accessibilityController.accessibleElementById("group4").childAtIndex(0).childAtIndex(0);
-        shouldBe("object.role", "'AXRole: AXStaticText'");
+        if (accessibilityController.platformName == "atk") {
+            object = accessibilityController.accessibleElementById("group4").childAtIndex(0);
+            shouldBe("object.role", "'AXRole: AXGroup'");
+        } else {
+            object = accessibilityController.accessibleElementById("group4").childAtIndex(0).childAtIndex(0);
+            shouldBe("object.role", "'AXRole: AXStaticText'");
+        }
         stringValue = object.stringValue.replace(/\n/g, '');
-        shouldBe("stringValue", "'AXValue:     text4  '");
+        if (accessibilityController.platformName == "atk")
+            shouldBe("stringValue", "'AXValue: text4'");
+        else
+            shouldBe("stringValue", "'AXValue:     text4  '");
 
         // When not all the parents have aria-hidden=false, element should not be visible.
         object = accessibilityController.accessibleElementById("group5").childAtIndex(0);
@@ -115,12 +130,18 @@ text3
 
         // Objects that don't have aria-hidden=false are not visible when the parents have that attribute.
         object = accessibilityController.accessibleElementById("group6").childAtIndex(0);
-        shouldBe("object.role", "'AXRole: AXGroup'");
+        if (accessibilityController.platformName == "atk")
+            shouldBe("object.role", "'AXRole: AXSection'");
+        else
+            shouldBe("object.role", "'AXRole: AXGroup'");
         shouldBe("object.childrenCount", "0");
 
         // When all objects have aria-hidden=false, then the elements are visible.
         object = accessibilityController.accessibleElementById("group7").childAtIndex(0);
-        shouldBe("object.role", "'AXRole: AXGroup'");
+        if (accessibilityController.platformName == "atk")
+            shouldBe("object.role", "'AXRole: AXSection'");
+        else
+            shouldBe("object.role", "'AXRole: AXGroup'");
         shouldBe("object.childrenCount", "1");
         shouldBe("object.childAtIndex(0).role", "'AXRole: AXButton'");
     }

LayoutTests/platform/gtk/TestExpectations
@@ -923,8 +923,6 @@ Bug(GTK) editing/spelling/markers.html [ Skip ]
 # Requires ENABLE(MEDIA_CAPTURE)
 webkit.org/b/85958 fast/forms/file/file-input-capture.html [ Failure ]
 
-webkit.org/b/146718 accessibility/aria-hidden-false-works-in-subtrees.html [ Failure ]
-
 webkit.org/b/146720 media/accessiblity-describes-video.html [ Failure ]
 
 webkit.org/b/146724 fast/text/word-break-keep-all.html [ ImageOnlyFailure ]

LayoutTests/platform/gtk/accessibility/aria-hidden-false-works-in-subtrees-expected.txt
@@ -1,18 +1,27 @@
-
 This tests that a sub-tree within a node marked with aria-hidden=false will be exposed in the AX tree.
 
 On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
 
 
 AXRole: AXSection
-AXDescription: group0
+AXTitle: group0
 
   AXRole: AXGroup
-  AXDescription: group2
+  AXTitle: group2
 
     AXRole: AXButton
-    AXDescription: button1
+    AXTitle: button1
 
+PASS object.role is 'AXRole: AXSection'
+PASS stringValue is 'AXValue: text3'
+PASS object.role is 'AXRole: AXGroup'
+PASS stringValue is 'AXValue: text4'
+PASS !object || !object.isValid is true
+PASS object.role is 'AXRole: AXSection'
+PASS object.childrenCount is 0
+PASS object.role is 'AXRole: AXSection'
+PASS object.childrenCount is 1
+PASS object.childAtIndex(0).role is 'AXRole: AXButton'
 Non-rendered iframe content should not be visible when aria-hidden=true. The first child should be a group and NOT static text.
 iFrame child role: AXRole: AXWebArea
 PASS successfullyParsed is true