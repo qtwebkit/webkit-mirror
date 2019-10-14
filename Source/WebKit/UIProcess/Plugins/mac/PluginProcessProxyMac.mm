qtwebkit/webkit-mirror

commit 9d5a45bf041f16054ac3ba3492f9233c7f3d17e5

Cherry-pick r250186. rdar://problem/55612001
    Safari 13 may launch leftover 32-bit plug-in process from Safari 12’s WebKit, which crashes
    https://bugs.webkit.org/show_bug.cgi?id=202077
    <rdar://problem/55547063>

    Reviewed by Sam Weinig.

    * Shared/Plugins/Netscape/mac/NetscapePluginModuleMac.mm:
    (WebKit::getPluginArchitecture): Removed support for i386 plug-ins.

    * UIProcess/Launcher/mac/ProcessLauncherMac.mm:
    (WebKit::serviceName): Assert that the requested process type is not Plugin32.

    * UIProcess/Plugins/mac/PluginProcessProxyMac.mm:
    (WebKit::PluginProcessProxy::platformGetLaunchOptionsWithAttributes): Assert that the
      plug-in architecture is x86_64.

    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@250186 268f45cc-cd09-0410-ab3c-d52691b4dbfc

git-svn-id: http://svn.webkit.org/repository/webkit/branches/safari-608-branch@250443 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/UIProcess/Plugins/mac/PluginProcessProxyMac.mm
@@ -60,10 +60,8 @@ namespace WebKit {
     
 void PluginProcessProxy::platformGetLaunchOptionsWithAttributes(ProcessLauncher::LaunchOptions& launchOptions, const PluginProcessAttributes& pluginProcessAttributes)
 {
-    if (pluginProcessAttributes.moduleInfo.pluginArchitecture == CPU_TYPE_X86)
-        launchOptions.processType = ProcessLauncher::ProcessType::Plugin32;
-    else
-        launchOptions.processType = ProcessLauncher::ProcessType::Plugin64;
+    ASSERT(pluginProcessAttributes.moduleInfo.pluginArchitecture == CPU_TYPE_X86);
+    launchOptions.processType = ProcessLauncher::ProcessType::Plugin64;
 
     launchOptions.extraInitializationData.add("plugin-path", pluginProcessAttributes.moduleInfo.path);
 

Source/WebKit/Shared/Plugins/Netscape/mac/NetscapePluginModuleMac.mm
@@ -60,12 +60,6 @@ static bool getPluginArchitecture(CFBundleRef bundle, PluginModuleInfo& plugin)
         plugin.pluginArchitecture = CPU_TYPE_X86_64;
         return true;
     }
-
-    // We also support 32-bit Intel plug-ins on 64-bit Intel.
-    if (architectures.contains(kCFBundleExecutableArchitectureI386)) {
-        plugin.pluginArchitecture = CPU_TYPE_X86;
-        return true;
-    }
 #endif
 
     return false;

Source/WebKit/UIProcess/Launcher/mac/ProcessLauncherMac.mm
@@ -58,7 +58,8 @@ static const char* serviceName(const ProcessLauncher::LaunchOptions& launchOptio
         return "com.apple.WebKit.Networking";
 #if ENABLE(NETSCAPE_PLUGIN_API)
     case ProcessLauncher::ProcessType::Plugin32:
-        return "com.apple.WebKit.Plugin.32";
+        ASSERT_NOT_REACHED();
+        return nullptr;
     case ProcessLauncher::ProcessType::Plugin64:
         return "com.apple.WebKit.Plugin.64";
 #endif

commit 363da8cada98b9bff95ffbc3471c00ed8043d586

Cherry-pick r250197. rdar://problem/55611999
    Fix an assertion failure introduced in r250186.

    * UIProcess/Plugins/mac/PluginProcessProxyMac.mm:
    (WebKit::PluginProcessProxy::platformGetLaunchOptionsWithAttributes): Corrected the
      assertion.

    git-svn-id: https://svn.webkit.org/repository/webkit/trunk@250197 268f45cc-cd09-0410-ab3c-d52691b4dbfc

git-svn-id: http://svn.webkit.org/repository/webkit/branches/safari-608.2.40.1-branch@251079 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/UIProcess/Plugins/mac/PluginProcessProxyMac.mm
@@ -60,7 +60,7 @@ namespace WebKit {
     
 void PluginProcessProxy::platformGetLaunchOptionsWithAttributes(ProcessLauncher::LaunchOptions& launchOptions, const PluginProcessAttributes& pluginProcessAttributes)
 {
-    ASSERT(pluginProcessAttributes.moduleInfo.pluginArchitecture == CPU_TYPE_X86);
+    ASSERT(pluginProcessAttributes.moduleInfo.pluginArchitecture == CPU_TYPE_X86_64);
     launchOptions.processType = ProcessLauncher::ProcessType::Plugin64;
 
     launchOptions.extraInitializationData.add("plugin-path", pluginProcessAttributes.moduleInfo.path);